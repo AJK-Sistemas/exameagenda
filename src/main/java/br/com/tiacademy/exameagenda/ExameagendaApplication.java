7af5ca3404b5132ef9e3795945cb0cf0d187d6d4
kellisgf <kellisgf@gmail.com>
2022-08-11 15:22:47 -0300

    Estrutura básica

---
+++ src/main/java/br/com/tiacademy/exameagenda/ExameagendaApplication.java
@@ -0,0 +1,13 @@
+package br.com.tiacademy.exameagenda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExameagendaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExameagendaApplication.class, args);
+	}
+
+}
